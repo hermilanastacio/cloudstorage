6973655e02869010faa2593ad9b903c3cafb6391
hermilanastacio <hermilanastacio@pinion.ph>
2020-10-06 13:46:34 +0800

    Updates

---
+++ precache-manifest.b9620fde65cfb6da52adb85a016b8930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce3512c64b5b654510c9297b4fbf28f",
+    "url": "/cloudstorage/index.html"
+  },
+  {
+    "revision": "e7cdde15b3f752b6d351",
+    "url": "/cloudstorage/static/css/main.dee62257.chunk.css"
+  },
+  {
+    "revision": "a56d8124248e5cf1c259",
+    "url": "/cloudstorage/static/js/2.5f48b180.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cloudstorage/static/js/2.5f48b180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7cdde15b3f752b6d351",
+    "url": "/cloudstorage/static/js/main.2c40e126.chunk.js"
+  },
+  {
+    "revision": "efbed47ff0034846d98f",
+    "url": "/cloudstorage/static/js/runtime-main.498fa401.js"
+  },
+  {
+    "revision": "9aed57e18f39cc8eedd7128e1eec08e6",
+    "url": "/cloudstorage/static/media/loading.9aed57e1.gif"
+  }
+]);
